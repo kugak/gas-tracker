0c8d148f25f07cc7c95ca8cb1e26932014049474
Kuga Kugaraja <contact@kuga.ca>
2019-11-01 14:08:02 -0400

    added express-handlebars

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 // initialize
 const app = express();
+
+// handlebars middlewares
+app.engine(
+  'handlebars',
+  exphbs({
+    defaultLayout: 'main'
+  })
+);
+app.set('view engine', 'handlebars');
 
 const port = 5000;
 
