40c2d95d47e0d2200052d547316e724e8f49cb6b
Kuga Kugaraja <contact@kuga.ca>
2019-11-01 15:45:40 -0400

    added moment

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+var Handlebars = require('handlebars');
+var MomentHandler = require('handlebars.moment');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
@@ -30,6 +32,9 @@ app.engine(
   })
 );
 app.set('view engine', 'handlebars');
+
+// moment format
+MomentHandler.registerHelpers(Handlebars); // <- this is the important line
 
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
